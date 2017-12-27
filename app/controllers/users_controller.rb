c61231ad745ac8dd0a6b8da75636726bb5656619
Le Hoa <lehoa543@gmail.com>
2017-12-27 16:22:04 +0700

    signup anh signin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
     @user = User.new user_params
     if @user.save
       flash[:success] = t :welcome
-      redirect_to @user
+      redirect_to root_url
     else
       render :new
     end
