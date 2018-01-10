1f02df25c4961b01e04c42678d7c628979f4c09f
Le Hoa <lehoa543@gmail.com>
2018-01-10 11:17:21 +0700

    signup signin gem devise

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -3,4 +3,11 @@ class Schedule < ApplicationRecord
   belongs_to :film
   has_many :orders
   scope :schedule_info, -> {select(:id, :time, :date, :price, :film_id )}
+
+  scope :find_time_by_date, (lambda do |film_id, date|
+    where(film_id: film_id, date: date)
+      .pluck(:id, :time)
+  end)
+
+  scope :find_date, ->(film_id){where(film_id: film_id).distinct.pluck(:date)}
 end
