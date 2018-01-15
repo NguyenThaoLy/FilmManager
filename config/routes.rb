5d13c74ffffec1bdf664a94e882178ac9d4b1e5c
Le Hoa <lehoa543@gmail.com>
2018-01-15 16:29:21 +0700

    search film use gem ransack

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   root "static_pages#home"
-  get  "/detailFilm", to: "static_pages#detailFilm"
-  get  "/priceTicket", to: "static_pages#priceTicket"
-  get  "/schedule", to: "static_pages#schedule"
+  get "/detailFilm", to: "static_pages#detailFilm"
+  get "/films", to: "static_pages#home"
+  get "/priceTicket", to: "static_pages#priceTicket"
+  get "/schedule", to: "static_pages#schedule"
   get "/bookticket", to: "static_pages#bookticket"
-  get "search", to: "searches#index"
-  get "search_results", to: "searches#show"
+  get "/search", to: "searches#index"
+  get "/search_results", to: "searches#show"
   devise_for :users
   resources :users
   resources :orders
